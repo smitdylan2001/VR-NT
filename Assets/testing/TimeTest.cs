0a542db0c251a6ef725c4583afd80f24c7ce11e9
Dylan Smit <dylan.smit01@gmail.com>
2020-10-09 23:21:12 +0200

    Got slowmo working

---
+++ Assets/testing/TimeTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTest : MonoBehaviour
+{
+    
+    public GameObject test;
+
+    void Start()
+    {
+        
+    }
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(1))
+		{
+            Time.timeScale /= 2;
+            Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        }
+	}
+
+	void fixedUpdate()
+    {
+        
+    }
+}
