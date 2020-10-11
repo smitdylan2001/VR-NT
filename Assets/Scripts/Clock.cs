de9603309899880f7e2e3a4e549e7f492e2c3fb5
Dylan Smit <dylan.smit01@gmail.com>
2020-10-11 12:56:08 +0200

    Added a clock
    With timer for time

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    private GameObject _rotatePoint;
+    private GameObject _clockPointer;
+
+    public int Times { get; private set; }
+    private bool _canIncreaseTimes;
+
+    void Start()
+    {
+        _rotatePoint = GameObject.Find("Rotatepoint");
+        _clockPointer = GameObject.Find("Pointer");
+        Times = 0;
+        _canIncreaseTimes = false;
+    }
+
+    void FixedUpdate()
+    {
+        Rotate();
+
+        TrackTime();
+    }
+
+    void Rotate()
+	{
+        _clockPointer.transform.RotateAround(_rotatePoint.transform.position, Vector3.back, Time.fixedDeltaTime * 25);
+
+    }
+
+    void TrackTime()
+	{
+        if (_clockPointer.transform.localRotation.eulerAngles.y > 0 && _clockPointer.transform.localRotation.eulerAngles.y < 30 && _canIncreaseTimes)
+        {
+            Times++;
+            _canIncreaseTimes = false;
+        }
+        if (_clockPointer.transform.localRotation.eulerAngles.y > 60 && !_canIncreaseTimes)
+        {
+            _canIncreaseTimes = true;
+        }
+    }
+}
---
+++ Assets/Scripts/TimeTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeTest : MonoBehaviour
+{
+    public GameObject rightController;
+    public GameObject leftController;
+    public Vector3 rightPosition;
+    public Vector3 leftPosition;
+    public Vector3 rightPositionCache;
+    public Vector3 leftPositionCache;
+    float motion;
+
+    void Start()
+    {
+        rightPosition = rightController.transform.position;
+        leftPosition = leftController.transform.position;
+
+        rightPositionCache = rightPosition;
+        leftPositionCache = leftPosition;
+    }
+
+    
+	private void Update()
+	{
+        
+	}
+
+	private void FixedUpdate()
+    {
+        rightPosition = rightController.transform.position;
+        leftPosition = leftController.transform.position;
+
+        motion = (((Mathf.Abs(rightPosition.x - rightPositionCache.x)) + (Mathf.Abs(rightPosition.y - rightPositionCache.y)) + (Mathf.Abs(rightPosition.z - rightPositionCache.z)) / 3) + ((Mathf.Abs(leftPosition.x - leftPositionCache.x)) + (Mathf.Abs(leftPosition.y - leftPositionCache.y)) + (Mathf.Abs(leftPosition.z - leftPositionCache.z)) / 3)) / 2;
+
+        rightPositionCache = rightPosition;
+        leftPositionCache = leftPosition;
+
+        Debug.Log(motion);
+
+        
+         Time.timeScale = Mathf.Lerp(Time.timeScale, 1 - (motion * 30), 0.05f);
+         Time.fixedDeltaTime = Time.timeScale * 0.02f;
+        
+    }
+}
